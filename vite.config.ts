07796a70ee52623b0735e5cfbf960b69e59db839
LittleShyStar02 <vincenzocutolo10@gmail.com>
2024-06-23 18:19:49 +0200

    click counter elements

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(
+{
   build: {
     outDir: 'build'
-  }
+  },
+  plugins: [react()],
+  server: { port: 8080 }
 })
